631081f465dcf54a6bbd43e865df9cb37c58568f
ngobrian004 <ngobrian@ufl.edu>
2023-04-24 19:13:46 -0400

    Made the optional classes

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -6,67 +6,6 @@ https://www.geeksforgeeks.org/program-sudoku-generator/
 
 """
 
-
-# It says that these classes are optional so I put them here,
-# but I am pretty sure they only are for the main shell so do them later
-class Cell:
-    def __init__(self, value, row, col, screen):
-        self.value = value
-        self.row = row
-        self.col = col
-        self.screen = screen
-
-    def set_cell_value(self, value):
-        self.value = value
-
-    def set_sketched_value(self, value):
-        pass
-
-    def draw(self):
-        pass
-
-
-class Board:
-    def __init__(self, width, height, screen, difficulty):
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.difficulty = difficulty
-
-    def draw(self):
-        pass
-
-    def select(self, row, col):
-        pass
-
-    def click(self, x, y):
-        pass
-
-    def clear(self):
-        pass
-
-    def sketch(self, value):
-        pass
-
-    def place_number(self, value):
-        pass
-
-    def reset_to_original(self):
-        pass
-
-    def is_full(self):
-        pass
-
-    def update_board(self):
-        pass
-
-    def find_empty(self):
-        pass
-
-    def check_board(self):
-        pass
-
-
 class SudokuGenerator:
     '''
 	create a sudoku board - initialize class variables and set up the 2D board
