88b7490d22a185bc200955e111f72a3b1d7ec873
ngobrian004 <ngobrian@ufl.edu>
2023-04-24 15:06:41 -0400

    Got the sudoku generator working and passed all tests.

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -86,7 +86,7 @@ class SudokuGenerator:
 
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
-        self.remove_cells = removed_cells
+        self.removed_cells = removed_cells
         self.board = []
         for i in range(row_length):
             row = []
@@ -168,7 +168,7 @@ class SudokuGenerator:
     '''
 
     def valid_in_box(self, row_start, col_start, num):
-        for row in range(row_start, row_start + 2):
+        for row in range(row_start, row_start + 3):
             for col in range(col_start, col_start + 3):
                 if self.board[row][col] == num:
                     return False
@@ -210,9 +210,9 @@ class SudokuGenerator:
         for i in range(row_start, row_start + 3):
             for j in range(col_start, col_start + 3):
                 while self.board[i][j] == 0:
-                    inputnum = random.randint(1,9)
-                    if self.is_valid(i, j, inputnum):
-                        self.board[i][j] = inputnum
+                    var = random.randint(1,9)
+                    if self.is_valid(i, j, var):
+                        self.board[i][j] = var
 
 
     '''
@@ -295,7 +295,19 @@ class SudokuGenerator:
     '''
 
     def remove_cells(self):
-        pass
+        cells = 0
+        random_row = random.randint(0,8)
+        random_col = random.randint(0,8)
+        while cells < self.removed_cells:
+            if self.board[random_row][random_col] != 0:
+                self.board[random_row][random_col] = 0
+                random_row = random.randint(0, 8)
+                random_col = random.randint(0, 8)
+                cells += 1
+            else:
+                random_row = random.randint(0, 8)
+                random_col = random.randint(0, 8)
+
 
 
 '''
@@ -322,3 +334,9 @@ def generate_sudoku(size, removed):
     sudoku.remove_cells()
     board = sudoku.get_board()
     return board
+
+
+sudoku = SudokuGenerator(9,0)
+sudoku.fill_values()
+sudoku.remove_cells()
+sudoku.print_board()
